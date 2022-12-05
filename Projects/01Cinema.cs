2bb64c02bb39fde2d360cabd7a9388271a5e3ac2
SlyCooperbg <119399662+SlyCooperbg@users.noreply.github.com>
2022-12-05 09:05:26 +0200

    Create 01Cinema.cs
    Adding a solution

---
+++ Projects/01Cinema.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01Cinema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pjctType = Console.ReadLine();
+            int r = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            switch (pjctType)
+            {
+                case "Premiere":
+                    Console.WriteLine("{0:F2}", r * c * 12);
+                    break;
+                case "Normal":
+                    Console.WriteLine("{0:F2}", r * c * 7.5);
+                    break;
+                case "Discount":
+                    Console.WriteLine("{0:F2}", r * c * 5);
+                    break;
+            }
+        }
+    }
+}
